e31d8c6f12d0f37dca490afae6879e54c606370a
Matteo Merola <mattmezza@gmail.com>
2017-01-11 14:26:45 +0100

    fixed brew formula

--- mannaggia.rb
+++ mannaggia.rb
@@ -5,7 +5,7 @@ class Mannaggia < Formula
   sha256 "844968a5662a2daab15a44e0c3f9667f8d3c4987a9581c812bc96dd232d45968"
 
   def install
-    ln_s "#{sbin}/mannaggia", "./mannaggia.sh"
+    ln_s "./mannaggia.sh", "#{sbin}/mannaggia"
   end
   test do
     quiet_system "#{sbin}/mannaggia", "-h"
